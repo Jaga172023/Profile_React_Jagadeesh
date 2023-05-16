fac0c4c4b259bffd8e439988aac3f05930bd8508
jaga172023 <jaga.101602@gmail.com>
2023-05-16 23:56:14 +0530

    Home Page Build

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,34 @@
+/**
+ * Font Usage : Arvo, Goodle fonts
+ * 
+ * Kindly find the font usage documentation with TailwindCSS from this link: https://nextjs.org/docs/pages/building-your-application/optimizing/fonts#with-tailwind-css
+ * 
+ */
+
+
+import NavBar from '@/components/navBar'
 import '@/styles/globals.css'
+import {Montserrat} from 'next/font/google'
+import Head from 'next/head'
+
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-mont',
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
+        <NavBar />
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
